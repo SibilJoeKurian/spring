cecc7238f5ab3d1b5d53eb7761a61894db08f15f
SibilJoeKurian <Sibil.kurian@gmail.com>
2020-06-24 21:07:37 -0400

    object injection

--- src/main/java/com/springDemo/demo/Triangle.java
+++ src/main/java/com/springDemo/demo/Triangle.java
@@ -1,34 +1,28 @@
 package com.springDemo.demo;
 
 public class Triangle {
-
     private
-    String type;
-
-
-
-    int height;
-    public Triangle(String type){
-        this.type=type;
-    }
-    public Triangle (int height){
-        this.height=height;
-    }
-    public Triangle(int height,String type){
-        this.height=height;
-        this.type=type;
-    }
-    public String getType() {
-        return type;
+    Point point1;
+    Point point2;
+    public Point getPoint2() {
+        return point2;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setPoint2(Point point2) {
+        this.point2 = point2;
     }
-    public int getHeight() {
-        return height;
+
+
+    public Point getPoint1() {
+        return point1;
     }
+
+    public void setPoint1(Point point1) {
+        this.point1 = point1;
+    }
+
     public void draw(){
-        System.out.println(getType()+" Drawing triangle "+getHeight());
+        System.out.println("The points are point1= "+getPoint1().getX()+" "+getPoint1().getY());
+        System.out.println("The points are point1= "+getPoint2().getX()+" "+getPoint2().getY());
     }
 }
